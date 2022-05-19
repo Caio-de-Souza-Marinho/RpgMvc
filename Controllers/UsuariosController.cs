726417bb95adfbb5a32d7698c165172686241458
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-05-19 20:50:54 -0300

    [FEAT] Adicionando controller de usuários

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RpgMvc.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public string uriBase = "xyz/Usuarios/";
+        //xyz tem que ser substituido pelo endereço da API.
+    }
+}
