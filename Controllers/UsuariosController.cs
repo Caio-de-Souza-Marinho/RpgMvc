d7a53d792d70c2a6adba8b98e785d2031c39dda8
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-05-19 22:39:16 -0300

    Adicionando método post para registrar novos usuários

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -1,10 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
+using RpgMvc.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace RpgMvc.Controllers
 {
     public class UsuariosController : Controller
     {
-        public string uriBase = "xyz/Usuarios/";
+        public string uriBase = "http://localhost:5000/Usuarios/";
         //xyz tem que ser substituido pelo endereço da API.
 
         [HttpGet]
@@ -12,5 +18,47 @@ namespace RpgMvc.Controllers
         {
             return View("CadastrarUsuario");
         }
+
+        [HttpPost]
+        public async Task<ActionResult> RegistrarAsync(UsuarioViewModel u)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string uriComplementar = "Registrar";
+
+                var content = new StringContent(JsonConvert.SerializeObject(u));
+                content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+                HttpResponseMessage response = await httpClient.PostAsync(uriBase + uriComplementar, content);
+
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    TempData["Mensagem"] = string.Format("Usuário {0} registrado com sucesso! Faça o login para acessar.", u.Username );
+                    return View("AutenticarUsuario");
+                }
+                else
+                {
+                    throw new System.Exception(serialized);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
