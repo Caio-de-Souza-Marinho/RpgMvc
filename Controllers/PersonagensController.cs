8958a3b96f9879d4cf1f16d98fee3b118b487b55
Caio de Souza Marinho <caiosouzamarinho@gmail.com>
2022-05-29 07:53:28 -0300

    Adicionando método CreateAsync

--- Controllers/PersonagensController.cs
+++ Controllers/PersonagensController.cs
@@ -11,7 +11,7 @@ namespace RpgMvc.Controllers
 {
     public class PersonagensController: Controller
     {
-        public string uriBase = "xyz/Personagens/";
+        public string uriBase = "http://localhost:5000/Personagens/";
         //xyz deve ser substituido pelo nome do seu site da API
 
         [HttpGet]
@@ -44,6 +44,34 @@ namespace RpgMvc.Controllers
             }
         }
 
+        [HttpPost]
+        public async Task<ActionResult> CreateAsync(PersonagemViewModel p)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string token = HttpContext.Session.GetString("SessionTokenUsuario");
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                var content = new StringContent(JsonConvert.SerializeObject(p));
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                HttpResponseMessage response = await httpClient.PostAsync(uriBase, content);  
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if(response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    TempData["Mensagem"] = string.Format("Personagem {0}, Id {1} salvo com sucesso!", p.Nome, serialized);
+                    return RedirectToAction("Index");
+                }
+                else
+                    throw new System.Exception(serialized);
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Create");
+            }
+        }
 
     }
 }
