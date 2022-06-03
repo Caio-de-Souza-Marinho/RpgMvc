583b402adb285fe0172776726403eef5e25bbd43
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-06-02 21:59:04 -0300

    Adicionando classe das Habilidades

---
+++ Models/HabilidadesViewModel.cs
@@ -0,0 +1,9 @@
+namespace RpgMvc.Models
+{
+    public class HabilidadesViewModel
+    {
+        public int Id { get; set; } 
+        public string Nome { get; set; }
+        public int Dano { get; set; }
+    }
+}
