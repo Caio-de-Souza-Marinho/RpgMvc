502df075ebc7cd0568e46dea5685c8ba21906564
Caio de Souza Marinho <caiosouzamarinho@gmail.com>
2022-06-06 07:26:46 -0300

    Adicionando método IndexDisputas

--- Controllers/DisputasController.cs
+++ Controllers/DisputasController.cs
@@ -159,6 +159,35 @@ namespace RpgMvc.Controllers
             }
         }
 
+        [HttpGet]
+        public async Task<ActionResult> IndexDisputasAsync()
+        {
+            try
+            {
+                string uriComplementar = "Listar";
+
+                HttpClient httpClient = new HttpClient();
+                string token = HttpContext.Session.GetString("SessionTokenUsuario");
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                HttpResponseMessage response = await httpClient.GetAsync(uriBase + uriComplementar);
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    List<DisputaViewModel> lista = await Task.Run(() => JsonConvert.DeserializeObject<List<DisputaViewModel>>(serialized));
+
+                    return View(lista);
+                }
+                else
+                    throw new System.Exception(serialized);
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
 
 
 
